ad9e5cb1a7e7235a840f5366d28800e55dc490fd
Open Humans Foundation <accounts+github@openhumans.org>
2017-10-23 22:38:07 -0700

    Fix introduced typo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 # Installing via gem file to use Netlify's Cache
-# gem install jekyll
+gem install jekyll
 
 # jekyll build
 mv _redirects _site
